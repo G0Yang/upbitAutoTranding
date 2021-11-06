feedec38a37fff78452765f5bdd1ef308c1e62f0
G0Yang <ender35841@gmail.com>
2021-11-06 18:11:37 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,9 +1,47 @@
 
+# configure .env
+```
+cp .env.sample .env
+```
+
+# make venv for dev
+```
+cd src
+python3 -m venv venv
+```
+
+# activate venv
+```
+cd src
+source venv/bin/activate
+```
+
+# deactivate venv
+```
+deactivate
+```
+
+# install requirements
+```
+python -m pip install -r requirements.txt
+```
+
+# run server on localhost
+```
+python runServer.py
+```
+
 # Build docker image
+```
 docker build -t upbit_auto_trading .
+```
 
 # Run api server
+```
 docker-compose up -d
+```
 
 # Stop api server
+```
 docker-compose down
+```
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.openapi.utils import get_openapi
+
 from routers.exchange import router as exchangeRouter
 from routers.quotation import router as quotationRouter
 
@@ -10,6 +12,24 @@ app = FastAPI()
 # add Routers
 app.include_router(exchangeRouter)
 app.include_router(quotationRouter)
+
+# set openapi
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="upbit auto trading",
+        version="0.0.1",
+        # description="upbit auto trading api document",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
 
 # root
 @app.get("/")
